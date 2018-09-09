16a6465f9689591983cc20a5ed43d6957fda9adc
SUMIT SAINI <sumitsaini7991@gmail.com>
2018-09-10 00:05:44 +0530

    added tutorialspoint link in notes of fop

--- app/src/main/java/com/anassolanki/root/lecturebook/FopActivity.java
+++ app/src/main/java/com/anassolanki/root/lecturebook/FopActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 
@@ -16,6 +18,7 @@ import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 
 public class FopActivity extends AppCompatActivity{
     ExpandableRelativeLayout ex_notes_btn,ex_ppt_btn,ex_books_btn;
+    private Button notes_tp;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +30,16 @@ public class FopActivity extends AppCompatActivity{
         actionBar.setDisplayHomeAsUpEnabled(false);
         actionBar.setTitle("FOP");
         actionBar.setDisplayShowTitleEnabled(true);
+
+        notes_tp= (Button)findViewById(R.id.notes_tp);
+
+        notes_tp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String urlsend="https://www.tutorialspoint.com/cprogramming/";
+                sender_fn_link(urlsend);
+            }
+        });
 
     }
 
@@ -49,6 +62,26 @@ public class FopActivity extends AppCompatActivity{
         ex_books_btn =(ExpandableRelativeLayout)findViewById(R.id.ex_imp_links_btn);
         ex_books_btn.toggle();
     }
+
+    public void sender_fn_link(String str){
+        String urlsend=str;
+
+        if(CheckNetwork.isInternetAvailable(this)) //returns true if internet available
+        {
+            Intent i=new Intent(this,NotesViewerActivity.class);
+            i.putExtra("urlsend",urlsend);
+            startActivity(i);
+            //do something. loadwebview.
+        }
+        else
+        {
+            Toast.makeText(this,"No Internet Connection",Toast.LENGTH_SHORT).show();
+        }
+
+
+        //finish();
+    }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
